d1b3ed0ad7460ac006ac61db92de6b0ee55c5917
Maurits van Riezen <MauritsVanRiezen@gmail.com>
2017-12-22 10:25:54 +0100

    Fixed image upload

--- blog/static/scripts/viewUser.js
+++ blog/static/scripts/viewUser.js
@@ -1,5 +1,11 @@
 $("#changeProfilePic").on('click', function(ev) {
     ev.preventDefault();
     $("#profilePicForm").css("display","unset");
+    /*$("#profilePicForm label").addClass("button");*/
+    $("#profilePicForm input").change(function() {
+        $("#profilePicForm").submit();
+    })
     $("#changeProfilePic").remove();
 })
+
+$("#profilePicForm label").addClass("button");
